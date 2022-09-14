d70f7fc3ef2aba94492b26e6ed56f29757c010b7
shrimeichock <meichock@gmail.com>
2022-09-14 19:38:05 -0400

    Add short job descriptions

--- src/Experiences.js
+++ src/Experiences.js
@@ -4,21 +4,21 @@ const experiences = [
         active: true,
         title: "Security Software Developer",
         company: "Nokia",
-        description: "Insert description"
+        description: "Providing training and development support to internal groups using the customized Keycloak software"
     },
     {
       key: 2,
       active: false,
       title: "Software Tester",
       company: "Nokia",
-      description: "Insert description"
+      description: "Wrote Python testcases using the in-house framework to automate the verification of user creation and maintenance API calls for the WaveSuite Common Platform (WSCP) product"
     },
     {
       key: 3,
-      active: true,
+      active: false,
       title: "IT Analyst/Developer",
       company: "Canada Revenue Agency",
-      description: "Insert description"
+      description: "Performed testing and troubleshooting for the SAP system to assist HR personnel management operations and documented code changes in the form of functional specifications"
     },
   ];
   
